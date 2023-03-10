87c6096cbe27985734965e2cae6c362a17081a39
whit <whitspa64@gmail.com>
2023-03-10 13:29:44 -0500

    added pages for the activities from which the users (resort guests) can choose

---
+++ client/src/pages/Ax.js
@@ -0,0 +1,13 @@
+//This page will provide details for resort guests who want to learn about the ax throwing activity
+import React from 'react';
+
+export default function Ax() {
+  return (
+    <div>
+      <h1>Ax</h1>
+      <p>
+       This is where we can display data and media about the Ax throwing activity
+      </p>
+    </div>
+  );
+}
---
+++ client/src/pages/Spa.js
@@ -0,0 +1,13 @@
+//This page will provide details for resort guests who want to learn about the spa activity
+import React from 'react';
+
+export default function Spa() {
+  return (
+    <div>
+      <h1>Spa</h1>
+      <p>
+       This is where we can display data and media about the spa activity
+      </p>
+    </div>
+  );
+}
