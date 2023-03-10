9cd339954a9dd7aaf74cf7a90b76a1782a41d8f8
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2023-03-10 18:45:02 -0500

    completed slash page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,25 @@
-import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import React, { useEffect,useState } from "react";
+import { Alert, Button} from "react-bootstrap";
 
 const Home = () => {
+  const[displayButton, setDisplayButton] = useState(true)
+  useEffect(function(){
+    setTimeout(function(){
+      setDisplayButton(false)
+    },2000)
+  },[displayButton])
+
+  
   return (
-    <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+    <div className="splash-page d-flex justify-content-center align-items-center">
+      {displayButton?
+      <Alert key="info" variant="info" className="alert-class">
+        Welcome to your Stay
+        </Alert>
+        : 
+        <Button variant="primary" size="lg" >
+        Press here to plan your stay!
+      </Button>}
     </div>
   );
 };
