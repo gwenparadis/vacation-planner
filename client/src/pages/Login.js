05bf8bd1174f8976cf69ea6d58abf8797ad77cdf
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2023-03-16 15:34:49 -0400

    added card style for sign up and log in

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,7 @@ import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
+import Card from 'react-bootstrap/Card';
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -33,33 +34,38 @@ function Login(props) {
 
   return (
     <div className="login">
-      {/* <Link to="/signup">← Go to Signup</Link> */}
-      <form onSubmit={handleFormSubmit}>
-        <h2>Welcome Back!</h2>
-        <Form.Group className="login-form" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-        <Link to="/signup">Create new account </Link>
-        <Button variant="warning display-center" type="submit">
-          Submit
-        </Button>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+      <Card className="m-2 p-5" style={{ width: '38rem' }}>
+        {/* <Link to="/signup">← Go to Signup</Link> */}
+        <h2 className="text-center">Welcome Back!</h2>
+        <form className="login-form" onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3 col-auto" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <div className="d-flex justify-content-between">
+            <Button variant="warning display-center" type="submit">
+              Submit
+            </Button>
+            <Link to="/signup">Create new account </Link>
           </div>
-        ) : null}
-      </form>
+          {error ? (
+            <div>
+              <p className="error-text">The provided credentials are incorrect</p>
+            </div>
+          ) : null}
+        </form>
+      </Card>
+
     </div>
   );
 }
