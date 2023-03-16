89f0e5a5a66b0bdd2e52e2880d5623101d429002
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2023-03-16 14:17:21 -0400

    adjusted nav bar spacing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -21,16 +21,16 @@ function Nav() {
     } else {
       return (
         <ul className="flex-row p-3">
-          <li className="mx-1">
+          <li className="mx-4">
             <Link to="/signup">Signup</Link>
           </li>
-          <li className="mx-1">
+          <li className="mx-4">
             <Link to="/login">Login</Link>
           </li>
-          <li className="mx-1">
+          <li className="mx-4">
             <Link to="/activities">Activities</Link>
           </li>
-          <li className="mx-1">
+          <li className="mx-4">
             <Link to="/profile">Profile</Link>
           </li>
         </ul>
@@ -39,7 +39,7 @@ function Nav() {
   }
 
   return (
-    <header className="flex-row px-1">
+    <header className="flex-row px-3">
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag"></span>
