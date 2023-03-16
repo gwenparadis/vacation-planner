a04327aaf18c920424da85a469a47bd685f1c375
gwenparadis <gwendelynparadis2@gmail.com>
2023-03-16 14:18:38 -0400

    footer in App.js

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <div className="footer-static">
-        <h6>&copy; Anu, Gwen, Tyler, Whit Productions, 2023</h6>
-     </div>
-  );
-}
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,22 +5,43 @@ const Header = ({ handleCurrentPage }) => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand onClick={() => { handleCurrentPage("Home") }} href="/">Vacation Planner</Navbar.Brand>
+        <Navbar.Brand
+          onClick={() => {
+            handleCurrentPage("Home");
+          }}
+          href="/"
+        >
+          Vacation Planner
+        </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link onClick={() => handleCurrentPage("Login")} href="#login">Login</Nav.Link>
-          <Nav.Link onClick={() => handleCurrentPage("Profile")} href="#profile">Profile</Nav.Link>
-          <Nav.Link onClick={() => handleCurrentPage("Signup")} href="#signup">Sign Up</Nav.Link>
-          <Nav.Link onClick={() => handleCurrentPage("Activity")} href="#activity">Activity</Nav.Link>
-          {/* <Navbar.Brand href="#login">Navbar with text</Navbar.Brand>
+          <Nav.Link onClick={() => handleCurrentPage("Login")} href="#login">
+            Login
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => handleCurrentPage("Profile")}
+            href="#profile"
+          >
+            Profile
+          </Nav.Link>
+          <Nav.Link onClick={() => handleCurrentPage("Signup")} href="#signup">
+            Sign Up
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => handleCurrentPage("Activity")}
+            href="#activity"
+          >
+            Activity
+          </Nav.Link>
+          <Navbar.Brand href="#login">Navbar with text</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
               Signed in as: <a href="#login">User</a>
             </Navbar.Text>
-          </Navbar.Collapse> */}
+          </Navbar.Collapse>
         </Nav>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 export default Header;
